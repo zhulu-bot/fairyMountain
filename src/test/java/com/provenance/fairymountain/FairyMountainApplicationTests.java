fc38b3d1f3a45e025283de589c98546d4b2ca5b8
zhuLu <2650202170@qq.com>
2023-01-10 13:53:25 +0800

    Initial commit

---
+++ src/test/java/com/provenance/fairymountain/FairyMountainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provenance.fairymountain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairyMountainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
